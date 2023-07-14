0a40623e97c1d93e5514a6b37e979ce2c75be79d
madix <madixmaleka@gmail.com>
2023-07-14 08:58:20 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,8 @@
  * main - Prints a random number and states whether 
  *        it is positive, negative, or zero.
  *
- * Return: Always 0 
- */
-
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
