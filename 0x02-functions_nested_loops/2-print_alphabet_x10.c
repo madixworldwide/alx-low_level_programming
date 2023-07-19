41a1849609d41fdabe58487ccda08ae85de59d92
madix <madixmaleka@gmail.com>
2023-07-19 04:57:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
+#include <stdio.h> // Add the include directive for stdio.h to access _putchar
 
 /**
  * print_alphabet_x10 - Print lowercase alphabet 10 times
  */
-
 void print_alphabet_x10(void)
 {
-	char num, letter;
+    char num, letter;
 
-	for (num = 0; num <= 9; num++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-	}
+    for (num = 0; num < 10; num++) // Fix the loop condition to avoid printing the letter 'z' 11 times
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter); // Assuming _putchar is defined elsewhere
+        }
+        _putchar('\n');
+    }
 }
 
