d320c75a10923af39e1dfa857a862f4063b2802b
madix <madixmaleka@gmail.com>
2023-07-20 05:00:01 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+/* File: 0-main.c */
+#include <stdio.h>
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+        /* Add a closing parenthesis for the return statement */
+        return (0);
+}
+
