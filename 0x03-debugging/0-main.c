a82a940ba9961cd235e54d8c84dcebef2851e36e
madix <madixmaleka@gmail.com>
2023-07-20 05:25:55 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
-/* File: 0-main.c */
-#include <stdio.h>
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-        int i;
+				int i;
 
-        printf("Infinite loop incoming :(\n");
+				int = 0;
+		positive_or_negative(i);
 
-        i = 0;
-        /* Add a closing parenthesis for the return statement */
-        return (0);
+		return (0);
 }
-
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* main - tests function that prints if integer is positive, negative, or zero
-* Return: 0
-*/
+#include <stdio.h>
 
-int main(void)
-{
-        int i;
+void positive_or_negative(int i);
 
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
-}
-
+#endif /* MAIN_H */
